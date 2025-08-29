98d3e2eaddd28b1bcc3bceb55757dc2f76ce7006
u221638 <ralf.winkelmann2@sbb.ch>
2025-08-29 15:17:38 +0200

    feat: finished SBBPrimaryButton (#4)

--- compose-mds/src/main/java/ch/sbb/compose_mds/composables/button/SBBPrimaryButton.kt
+++ compose-mds/src/main/java/ch/sbb/compose_mds/composables/button/SBBPrimaryButton.kt
@@ -1,5 +1,6 @@
-package ch.sbb.compose_mds.beta.button
+package ch.sbb.compose_mds.composables.button
 
+import SBBTheme
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.height
@@ -13,36 +14,52 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import ch.sbb.compose_mds.beta.ExperimentalSBBComponent
+import ch.sbb.compose_mds.composables.loadingIndicator.SBBLoadingIndicator
 
-@ExperimentalSBBComponent
+/***
+ * Implementation of the SBB Primary Button.
+ *
+ * @param label label of button
+ * @param enabled controls the enabled state of this button
+ * @param isLoading show loading indicator instead of [label] and disables button
+ * @param onClick called when this button is clicked
+ *
+ * For a complete definition of the component, please visit [digital.sbb.ch](https://digital.sbb.ch/de/design-system/mobile/components/button/)
+ */
 @Composable
 fun SBBPrimaryButton(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
+    isLoading: Boolean = false,
     label: String,
     onClick: () -> Unit,
 ) {
     val interactionSource = remember { MutableInteractionSource() }
     val pressed by interactionSource.collectIsPressedAsState()
+    val buttonColors = buttonColors(pressed)
 
     Button(
         modifier = modifier.height(44.dp),
-        enabled = enabled,
+        enabled = enabled && !isLoading,
         onClick = onClick,
         shape = RoundedCornerShape(22.dp),
-        colors = buttonColors(pressed),
+        colors = buttonColors,
         interactionSource = interactionSource,
     ) {
-        Text(
-            text = label,
-            style = SBBTheme.materialTypography.bodyMedium,
-        )
+        if (isLoading) {
+            SBBLoadingIndicator.Small(color = buttonColors.disabledContentColor)
+        } else {
+            Text(
+                text = label,
+                style = SBBTheme.materialTypography.bodyMedium,
+            )
+        }
     }
 }
 
 @Composable
-private fun buttonColors(pressed: Boolean) = if (SBBTheme.isDarkMode) darkModeColors(pressed) else lightModeColors(pressed)
+private fun buttonColors(pressed: Boolean) =
+    if (SBBTheme.isDarkMode) darkModeColors(pressed) else lightModeColors(pressed)
 
 @Composable
 private fun lightModeColors(pressed: Boolean): ButtonColors {
